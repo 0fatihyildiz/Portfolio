1f50b96dbc9da7b779a0cc30edba843694c0175b
Fatih Yıldız <47571500+0fatihyildiz@users.noreply.github.com>
2023-04-16 02:11:04 +0300

    feat: added noise effect

--- app.vue
+++ app.vue
@@ -4,19 +4,22 @@ import '@unocss/reset/tailwind.css'
 
 <template>
   <main>
+    <Noise />
     <div class="content">
       <Header />
       <Hero />
+      <Stats />
+      <Works />
     </div>
   </main>
 </template>
 
 <style lang="postcss" scoped>
 main {
-  @apply bg-gray-50 rounded-2xl h-full w-full py-6 px-4;
+  @apply bg-gray-50 border border-12 border-yellow-300 h-full w-full py-6 px-4 overflow-y-auto;
 
   .content {
-    @apply max-w-4xl mx-auto;
+    @apply max-w-6xl mx-auto;
   }
 }
 </style>
---
+++ components/noise.vue
@@ -0,0 +1,45 @@
+<script lang="ts" setup>
+const canvas = ref<HTMLCanvasElement | undefined>()
+const loop = ref(true)
+
+function resize() {
+  const w = window.innerWidth
+  const h = window.innerHeight
+  canvas.value?.setAttribute('width', w.toString())
+  canvas.value?.setAttribute('height', h.toString())
+}
+
+function noise(canvas: HTMLCanvasElement | undefined) {
+  const noiseFrame = canvas
+  const ctx = noiseFrame?.getContext('2d')
+  const w = ctx?.canvas.width || 0
+  const h = ctx?.canvas.height || 0
+  const idata = ctx?.createImageData(w, h)
+  const buffer32 = new Uint32Array(idata?.data.buffer as ArrayBuffer)
+  const len = buffer32.length
+
+  for (let i = 0; i < len; i++)
+    buffer32[i++] = 0xFF000000 | (Math.random() * 0x00FFFFFF)
+  ctx?.putImageData(idata as ImageData, 0, 0)
+}
+
+onMounted(() => {
+  (function loopFrame() {
+    loop.value = !loop.value
+    if (loop.value) {
+      requestAnimationFrame(loopFrame)
+      return
+    }
+    noise(canvas.value)
+    requestAnimationFrame(loopFrame)
+  })()
+  window.addEventListener('resize', resize)
+  resize()
+})
+</script>
+
+<template>
+  <teleport to="body">
+    <canvas ref="canvas" class="z-20 fixed opacity-20 pointer-events-none select-none" />
+  </teleport>
+</template>
