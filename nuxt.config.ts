c4f98b41c6968ae808eb894cd221a8557e443c87
Fatih Yıldız <47571500+0fatihyildiz@users.noreply.github.com>
2023-04-16 02:36:28 +0300

    chore: seo updated

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,19 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  app: {
+    head: {
+      title: 'Fatih',
+      meta: [
+        { charset: 'utf-8' },
+        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+        { hid: 'description', name: 'description', content: '' },
+        { name: 'format-detection', content: 'telephone=no' },
+      ],
+      link: [
+        { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      ],
+    },
+  },
   css: ['/assets/main.css'],
   modules: [
     '@unocss/nuxt',
