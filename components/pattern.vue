bbf37e2af3ff81962217d1bcf6b63c0d9332a986
Fatih Yıldız <47571500+0fatihyildiz@users.noreply.github.com>
2023-04-17 01:32:57 +0300

    feat: added patterns

---
+++ components/pattern.vue
@@ -0,0 +1,19 @@
+<script lang="ts" setup>
+import { defineAsyncComponent } from 'vue'
+
+const props = defineProps<{
+  name?: string
+  height?: number
+  width?: number
+}>()
+
+const random = Math.floor(Math.random() * 60) + 1
+
+const icon = defineAsyncComponent(() =>
+  import(`../assets/patterns/${props.name ? props.name : random}.svg`),
+)
+</script>
+
+<template>
+  <component :is="icon" viewBox="0 0 32 32" :heigh="height" :width="width" />
+</template>
