b9f8c1d434234e5f7cdfde23669de85ce6cde660
Fatih Yıldız <47571500+0fatihyildiz@users.noreply.github.com>
2023-04-18 16:04:42 +0300

    feat: cta added

---
+++ components/cta.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="my-24 relative isolate overflow-hidden bg-gray-900 px-6 pt-16 shadow-2xl sm:rounded-3xl sm:px-16 md:pt-24 lg:flex lg:gap-x-20 lg:px-24 lg:pt-0">
+    <div class="absolute left-1/2 top-2/3 -z-10 h-[64rem] w-[64rem] -translate-y-1/3 sm:left-full sm:-ml-80 lg:left-1/2 lg:ml-0 lg:-translate-x-1/2 lg:translate-y-0 opacity-[0.2] blur-xl rounded-full bg-gradient-to-r from-[#7775D6] to-[#E935C1]" />
+    <div class="mx-auto text-center lg:py-32">
+      <h2 class="text-5xl font-bold tracking-tight text-white">
+        Want to start a project?
+      </h2>
+      <p class="mt-6 text-lg leading-8 text-gray-300 max-w-xl">
+        Ac euismod vel sit maecenas id pellentesque eu sed consectetur. Malesuada adipiscing sagittis vel nulla.
+      </p>
+      <div class="mt-10 flex items-center justify-center gap-x-6">
+        <a href="" class="btn border border-white">Let's Talk to me</a>
+      </div>
+    </div>
+  </div>
+</template>
