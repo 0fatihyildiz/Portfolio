307e97d47908484326b8a1603dbf5fec84042a44
Fatih Yıldız <47571500+0fatihyildiz@users.noreply.github.com>
2023-04-18 16:05:05 +0300

    feat: Created config

---
+++ composables/config.ts
@@ -0,0 +1,21 @@
+const config = {
+  profile_photo: '/images/profile.jpg',
+  name: 'Fatih Yildiz',
+  job_title: 'Frontend Developer',
+  email: '0m.fatih.yildiz@gmail.com',
+  description:
+    'I am a frontend developer who loves to build web applications with Vue.js and Nuxt.js.',
+  socials: [
+    {
+      name: 'Github',
+      url: 'https://github.com/0fatihyildiz',
+    },
+    {
+      name: 'Twitter',
+      url: 'https://twitter.com/0fatihyildiz',
+    },
+  ],
+  work_provide: ['Github', 'Dribbble'],
+}
+
+export default config
