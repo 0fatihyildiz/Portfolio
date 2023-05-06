31b84dfb04f384e62abecb9d5b27df2416ac9973
Fatih Yıldız <47571500+0fatihyildiz@users.noreply.github.com>
2023-05-07 01:19:21 +0300

    feat(hero): in-view animation

--- components/hero.vue
+++ components/hero.vue
@@ -1,22 +1,28 @@
+<script setup lang="ts">
+import { animateKeyframes, initialKeyframes, transition } from '@/composables/animation'
+</script>
+
 <template>
   <section class="relative">
-    <div class="pattern -translate-y-1/2 absolute top-2/3 -right-80">
-      <Pattern class="h-6 w-6 text-zinc-950" />
-    </div>
-    <div class="pattern -translate-y-1/2 absolute top-2/3 -left-80">
-      <Pattern class="h-6 w-6 text-zinc-950" />
-    </div>
     <div class="flex flex-col items-center space-y-4 py-24">
-      <h1 class="title highlight">
-        Hi, I'm <span class="text-zinc-900">Fatih Yildiz</span>
-      </h1>
-      <h1 class="title !text-5xl">
-        I'm a <span class="text-zinc-900">Frontend Developer</span> from
-        <span class="text-zinc-900">Turkey</span>
-      </h1>
-      <p class="subtitle">
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Placeat, et delectus. Maiores dicta, deleniti eos et iusto alias mollitia officiis praesentium rerum explicabo autem ratione optio, dolorem, saepe repudiandae voluptas!
-      </p>
+      <PresenceGroup>
+        <Motion :initial="initialKeyframes" :in-view="animateKeyframes" :transition="transition(0.3)">
+          <h1 class="title highlight">
+            Hi, I'm <span class="text-zinc-900">Fatih Yildiz</span>
+          </h1>
+        </Motion>
+        <Motion :initial="initialKeyframes" :in-view="animateKeyframes" :transition="transition(0.4)">
+          <h1 class="title !text-5xl">
+            I'm a <span class="text-zinc-900">Frontend Developer</span> from
+            <span class="text-zinc-900">Turkey</span>
+          </h1>
+        </Motion>
+        <Motion :initial="initialKeyframes" :in-view="animateKeyframes" :transition="transition(0.5)">
+          <p class="subtitle">
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Placeat, et delectus. Maiores dicta, deleniti eos et iusto alias mollitia officiis praesentium rerum explicabo autem ratione optio, dolorem, saepe repudiandae voluptas!
+          </p>
+        </Motion>
+      </PresenceGroup>
     </div>
   </section>
 </template>
