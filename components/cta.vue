d404c711c2f1d5661074a2f93a4e5cec73d1cf52
Fatih Yıldız <47571500+0fatihyildiz@users.noreply.github.com>
2023-05-07 01:22:20 +0300

    style(cta): some changes

--- components/cta.vue
+++ components/cta.vue
@@ -1,13 +1,6 @@
-<script setup lang="ts">
-const video = ref<HTMLVideoElement>()
-onMounted(() => {
-  video.value?.play()
-})
-</script>
-
 <template>
   <div class="my-24 relative isolate overflow-hidden bg-gray-900 pt-16 shadow-2xl sm:rounded-3xl md:pt-24 lg:flex lg:gap-x-20 lg:pt-0">
-    <div class="mx-auto text-center lg:py-32 bg-black/60 w-full px-6 sm:px-16 lg:px-24">
+    <div class="mx-auto text-center lg:py-32 bg-black/70 w-full px-6 sm:px-16 lg:px-24">
       <h2 class="text-5xl font-bold tracking-tight text-white">
         Want to start a project?
       </h2>
@@ -18,6 +11,6 @@ onMounted(() => {
         <a href="" class="btn border border-white">Let's Talk to me</a>
       </div>
     </div>
-    <video ref="video" src="@/assets/videos/want.mp4" class="w-full h-full object-cover absolute left-1/2 -translate-x-1/2 -z-10" autoplay loop />
+    <img src="@/assets/videos/want.gif" class="w-full h-full object-cover absolute left-1/2 -translate-x-1/2 -z-10">
   </div>
 </template>
