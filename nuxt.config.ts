f8bf526b189948af85570990cc95a379c6ad5105
Fatih Yıldız <47571500+0fatihyildiz@users.noreply.github.com>
2023-06-02 02:00:18 +0300

    chore: deploy problem

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,5 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  experimental: {
-    watcher: 'parcel',
-  },
   app: {
     head: {
       title: 'Fatih',
